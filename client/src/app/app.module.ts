c7703c29c7c3082d4c27e3e6a4ecdcbc49f5e0b6
Hassan <hassanalihazaraa@gmail.com>
2020-09-07 12:19:54 +0200

    moved form to separate component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FriendFormComponent } from './friend-form/friend-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FriendFormComponent
   ],
   imports: [
     BrowserModule,
