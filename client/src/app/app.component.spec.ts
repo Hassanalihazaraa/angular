02232cb3b513f1c0484b1b76ac0c4d4a923c58f1
Hassan <hassanalihazaraa@gmail.com>
2020-09-07 11:55:29 +0200

    added header and footer

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'form'`, () => {
+  it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('form');
+    expect(app.title).toEqual('client');
   });
 
   it('should render title', () => {
